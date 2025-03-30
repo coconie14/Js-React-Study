29f7ef0dde402b3dbd850501ed8207470f787fa4
Coni <hicizc@gmail.com>
2025-03-30 19:19:46 +0900

    section05

---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,20 @@
+const Button = ({children, text, color = "black"}) => {
+    const onClickButton = () =>{
+        console.log(text);
+    }
+    
+    return (
+        <button 
+            onClick={onClickButton}
+            // onMouseEnter={onClickButton}
+        style={{color : color}}>
+            {text} - {color.toUpperCase()}
+            {children}
+        </button>
+    )
+    
+};
+
+
+
+export default Button;
