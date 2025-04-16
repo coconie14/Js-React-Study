e278bd38794749863b857e4e91454cad47260a04
Coni <hicizc@gmail.com>
2025-04-16 09:06:46 +0900

    section07

--- section06-hw/src/App.jsx
+++ section06-hw/src/App.jsx
@@ -1,15 +1,27 @@
 
 import './App.css'
+import Viwer from './components/Viewer'
+import Controller from './components/Controller'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
 
 
   return (
-    <>
+    <div className='App'>
       <h1>Simple Counter</h1>
-    </>
+        <section>
+          <Viwer count ={count}/>
+        </section>
+        <section>
+          <Controller onClickButton={onClickButton}/>
+        </section>
+    </div>
   )
 }
 
 export default App
-  
